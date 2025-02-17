251dec7ec89c4c232808014115ba3b7e29986672
yangmengbo <yangmengbo22@csu.ac.cn>
2025-02-17 14:01:56 +0800

    chore: update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,101 +1,25 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import prettier from 'eslint-plugin-prettier'
-import unicorn from 'eslint-plugin-unicorn'
-import globals from 'globals'
-import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
-
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    ignores: ['dist/', 'vite-env.d.ts', 'package-lock.json', '.idea/', '.husky/', 'demo/', 'eslint.config.js'],
-  },
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended-type-checked',
-      'prettier',
-      'plugin:unicorn/recommended',
-      'plugin:react/recommended',
-      'plugin:react/jsx-runtime',
-    ),
-  ),
-  {
-    plugins: {
-      'react-refresh': reactRefresh,
-      prettier,
-      unicorn: fixupPluginRules(unicorn),
-    },
-
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-
-      parserOptions: {
-        project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.build.json'],
-        tsconfigRootDir: '.',
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
-
     rules: {
-      '@typescript-eslint/ban-ts-comment': 'off',
-      'unicorn/prevent-abbreviations': 'off',
-      'linebreak-style': ['error', 'unix'],
-
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          args: 'all',
-          argsIgnorePattern: '^_',
-          caughtErrors: 'all',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-        },
-      ],
-
-      'no-debugger': 'error',
-
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        {
-          fixStyle: 'inline-type-imports',
-        },
-      ],
-
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      'no-console': 'error',
-      eqeqeq: ['error', 'always'],
-      'arrow-body-style': ['error', 'as-needed'],
-      'unicorn/no-null': 'off',
-      'unicorn/prefer-module': 'off',
-      'react/prop-types': 'off',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-]
+)
